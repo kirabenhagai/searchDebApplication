f39952f1f3d1230a07628da1e19500b945582e31
kira <kira@sears.co.il>
2017-01-16 22:15:28 +0200

    cleaning

---
+++ Domain/UriProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using myWebApplication.Configurations;
+
+namespace myWebApplication.Domain
+{
+	public class UriProvider
+	{
+		public Uri GetSearchUri(string search, ApplicationSettings settings)
+		{
+			return new Uri($"{settings.SearchApiUrl}?q={search}&limit=20&token={settings.AppToken}&hash={settings.AppHash}");
+		}
+
+		public Uri GetProductUrl(int productId, ApplicationSettings settings)
+		{
+			return new Uri($"{settings.GetProductUrl}?ids={productId}&token={settings.AppToken}&hash={settings.AppHash}");
+		}
+	}
+}
